ef6f8ff3e9097e21f1d5ec6451a89d1188580d2b
Ilya.Veselov <Ilya.Veselov@5cef0512-0c1d-acea-225d-32fde4969eed>
2009-03-25 14:43:16 +0000

    Issue 1: Generated .reg file is incomplete
    ! hacked Register.reg file generation
    
    also
    ! fixed comment building

--- TfsProvider/TfsProvider.cs
+++ TfsProvider/TfsProvider.cs
@@ -24,7 +24,7 @@ namespace TsvnTfsProvider
 			var form = new IssuesBrowser(originalMessage);
 			if (form.ShowDialog() != DialogResult.OK) return originalMessage;
 
-			var result = new StringBuilder(form.Comment);
+			var result = new StringBuilder();
 			if (originalMessage.Length != 0 && !originalMessage.EndsWith("\n")) result.AppendLine();
 
 			foreach (var workItem in form.AssociatedWorkItems)
@@ -32,6 +32,8 @@ namespace TsvnTfsProvider
 				result.AppendFormat("{0} {1}: {2}", workItem.type, workItem.id, workItem.title);
 				result.AppendLine();
 			}
+			result.AppendLine();
+			result.AppendLine(form.Comment);
 
 			return result.ToString();
 		}
